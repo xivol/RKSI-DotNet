d2cc059357259588588365f2d48c813918f2bb54
xivol <loshkarev.i@gmail.com>
2019-02-03 16:44:39 +0300

    Lab5

---
+++ Lab5/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Lab5 
+ * Классы. Наследование
+ * 
+ * Часть 1. Реализовать в классе Student метод считывания данных из потока ввода.
+ *          Считать данные группы студентов из файла.
+ *          Вычислить число лиц мужского, женского и других полов.
+ * 
+ * Часть 2. Реализовать
+ */
+using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student("Иван", "Иванов", "Иванович", Gender.Male, "ПОКС", 1 ,2);
+
+            // ToString() вызывается автоматически при преобразовании к строке
+            Console.WriteLine(stud);
+            // ToString() - виртуальная функция: будет позднее связывание
+            Console.WriteLine(stud as Person);
+
+            // FullName - не виртуальное свойство: будет раннее связывание
+            Console.WriteLine(stud.FullName);
+            Console.WriteLine((stud as Person).FullName);
+
+            Person pers = Person.Read(Console.In);
+            Student stud2 = new Student(pers, "БТ", 3, 1);
+            Console.WriteLine(stud2);
+        }
+    }
+}
---
+++ Lab5/Student.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Lab5 
+ * Классы. Наследование
+ * 
+ * Часть 1. Реализовать в классе Student метод считывания данных из потока ввода.
+ *          Считать данные группы студентов из файла.
+ *          Вычислить число лиц мужского, женского и других полов.
+ * 
+ * Часть 2. Реализовать
+ */
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Класс студент
+    /// </summary>
+    public class Student : Person
+    {
+        protected uint course;
+        protected uint group;
+        protected string track;
+
+        public Student(string firstname, string lastname, string patronim, Gender gender,
+                       string track, uint course, uint group) : base(gender, firstname, lastname, patronim)
+        {
+            this.track = track;
+            this.course = course;
+            this.group = group;
+        }
+
+        public Student(Person p, string track, uint course, uint group) : 
+            this(p.Firstname, p.Lastname, p.Patronim, p.Gender, track, course, group)
+        {}
+
+        /// <summary>
+        /// Read the specified input.
+        /// </summary>
+        /// <returns>The read.</returns>
+        /// <param name="input">Input.</param>
+        // TODO: public static new Student Read(TextReader input);
+
+        /// <summary>
+        /// Курс
+        /// </summary>
+        /// <value> Номер курса</value>
+        public uint Course
+        {
+            get { return this.course; }
+        }
+
+        // Номер группы
+        public uint Group
+        {
+            get { return this.group; }
+        }
+
+        // Учебная программа
+        public string Track
+        {
+            get { return this.track; }
+        }
+
+        // Название группы
+        public string GetGroupName()
+        {
+            // Пример работы со StringBuilder
+            StringBuilder result = new StringBuilder();
+
+            result.Append(track).Append("-").Append(course).Append(group);
+
+            return result.ToString();
+        }
+
+
+        public new string FullName // закрывает метод базового класса Person
+        {
+            get
+            {
+                return this.lastname + " " + this.firstname;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}; {GetGroupName()}";
+        }
+    }
+}
